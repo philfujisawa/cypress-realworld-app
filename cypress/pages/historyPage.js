782462437341c57cf028df1421c72f7bd69e8b04
Philippe Fujisawa <philfujisawa@gmail.com>
2024-11-25 21:15:55 -0300

    Fix CI

--- cypress/pages/historyPage.js
+++ cypress/pages/historyPage.js
@@ -10,12 +10,12 @@ class HistoryPage {
     }
 
     checkAllHistory () {
-        cy.get(this.selectorsList().everyoneButton).eq(0).click()
+        cy.get(this.selectorsList().everyoneButton).eq(0).click( { force: true } )
         cy.get(this.selectorsList().allTransaction).should('be.visible')
     }
 
     checkMineHistory () {
-        cy.get(this.selectorsList().mineButton).click()
+        cy.get(this.selectorsList().mineButton).click( { force: true } )
         cy.location('pathname').should('equal', "/personal")
     }
    
